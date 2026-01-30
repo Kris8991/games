6883fb11d3be0530819ec7934db67b921874d778
Kristina Natyrova <ogw72.98@mail.ru>
2026-01-30 12:23:30 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import MemoryGrid from "./components/MemoryGame";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <MemoryGrid />
+    </div>
+  );
+};
+
+export default App;
